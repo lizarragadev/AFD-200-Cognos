2cb5ab2e062a1b653a9956de962141ae33d083c1
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-08-16 22:26:17 -0400

    Add stateless sample

--- lesson05_stateless/lib/src/screens/widgets_screen.dart
+++ lesson05_stateless/lib/src/screens/widgets_screen.dart
@@ -14,38 +14,60 @@ class WidgetsScreen extends StatelessWidget {
   }
 
   Widget body() {
-    return const Column(
+    return Column(
       children: [
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Text(
+        const Text(
           "Curso de Flutter en Cognos Capacitación",
           style: TextStyle(
               fontSize: 20, color: Colors.blue, fontFamily: "Times New Roman"),
         ),
-        SizedBox(
+        const SizedBox(
           height: 10,
         ),
-        Text(
+        const Text(
           "Flutter Development",
           style:
               TextStyle(fontSize: 30, color: Colors.green, fontFamily: "Arial"),
         ),
-        SizedBox(
+        const SizedBox(
           height: 20,
         ),
-        Image(
+        const Image(
           image: AssetImage("assets/images/imagen.png"),
           width: 200,
         ),
-        SizedBox(height: 10),
-        Image(
+        const SizedBox(height: 10),
+        const Image(
           image: NetworkImage(
               "https://cdn.create.vista.com/api/media/small/110016664/stock-photo-close-up-of-a-tricolor"),
           width: 200,
         ),
+        const SizedBox(height: 10),
+        cardPersonalizado()
       ],
+    );
+  }
+
+  Widget cardPersonalizado() {
+    return const Card(
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Row(
+          children: [
+            Image(
+              image: AssetImage("assets/images/img.png"),
+              width: 50,
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Text("Galeria de fotos", style: TextStyle(fontSize: 30))
+          ],
+        ),
+      ),
     );
   }
 }
