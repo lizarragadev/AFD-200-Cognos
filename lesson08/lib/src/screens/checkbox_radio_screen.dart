d29e3f068a90cb2322d6b87b64c846a3dd0a4e78
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-08-28 20:36:05 -0400

    Add lesson 8

--- lesson08/lib/src/screens/checkbox_radio_screen.dart
+++ lesson08/lib/src/screens/checkbox_radio_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:grouped_buttons_ns/grouped_buttons_ns.dart';
 
 class CheckboxRadioScreen extends StatefulWidget {
   const CheckboxRadioScreen({ Key? key }) : super(key: key);
@@ -9,13 +10,46 @@ class CheckboxRadioScreen extends StatefulWidget {
 }
 
 class _CheckboxRadioScreenState extends State<CheckboxRadioScreen> {
+  final elemCheckBox = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+  List<String> checkSeleccionados = [];
+
+  final elemRadio = ["Masculino", "Femenino", "No binario"];
+  String radioSeleccionado = "";
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CheckBoxes & Radio Buttons'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text("Seleccione los días disponibles:"),
+            CheckboxGroup(
+              labels: elemCheckBox,
+              onSelected: (List<String> seleccionados) {
+                setState(() {
+                  checkSeleccionados = seleccionados;
+                });
+              },
+            ),
+            Text("Seleccionados: $checkSeleccionados"),
+            const SizedBox(height: 20,),
+            const Text("Seleccione su género:"),
+            RadioButtonGroup(
+              labels: elemRadio,
+              onSelected: (seleccionado) {
+                setState(() {
+                  radioSeleccionado = seleccionado;
+                });
+              },
+            ),
+            Text("Opción seleccionada: $radioSeleccionado")
+          ],
+        ),
+      ),
     );
   }
 }
--- lesson08/lib/src/screens/textfields_screen.dart
+++ lesson08/lib/src/screens/textfields_screen.dart
@@ -8,13 +8,79 @@ class TextfieldsScreen extends StatefulWidget {
 }
 
 class _TextfieldsScreenState extends State<TextfieldsScreen> {
+  
+  String valorTextField = "";
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Textfields'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              const Text("TextField Normal"),
+              const TextField(
+                autofocus: true,
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 20,),
+              const Text("TextField con tipo de teclado"),
+              const TextField(
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 20,),
+              const Text("TextField con tamaño max y capitalizacion"),
+              const TextField(
+                textInputAction: TextInputAction.next,
+                maxLength: 10,
+                textCapitalization: TextCapitalization.words,
+              ),
+              const SizedBox(height: 20,),
+              const Text("TextField tipo Password"),
+              const TextField(
+                textInputAction: TextInputAction.next,
+                obscureText: true,
+              ),
+              const SizedBox(height: 20,),
+              const Text("Detectar cambios"),
+              TextField(
+                onChanged: (valor) {
+                  setState(() {
+                    valorTextField = valor;
+                  });
+                },
+                textInputAction: TextInputAction.next,
+              ), 
+              const SizedBox(height: 20,),
+              Text("Valor detectado: $valorTextField"),
+              const SizedBox(height: 20,),
+              const Text("TextField Decoración"),
+              const SizedBox(height: 20,),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Ingrese su nombre",
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 0, bottom: 0, left: 20, right: 20),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    borderSide: BorderSide(color: Colors.green, width: 2)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    borderSide: BorderSide(color: Colors.grey, width: 1)
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
