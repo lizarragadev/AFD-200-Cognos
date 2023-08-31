61c141eaa1f1bd28b8ce01c3010420178218a917
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-08-30 22:24:10 -0400

    add final code lesson 8 lab

--- lesson08_lab/lib/src/screens/home_screen.dart
+++ lesson08_lab/lib/src/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_buttons_ns/grouped_buttons_ns.dart';
+import 'package:lesson08_lab/src/routes/routes.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -8,13 +10,177 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime dateCheckIn = DateTime.now();
+  DateTime dateCheckOut = DateTime.now();
+  final int CHECK_IN = 1;
+  final int CHECK_OUT = 2;
+  int numeroDeAdultos = 0;
+  int numeroDeNinios = 0;
+  
+  var textStyleOrange = const TextStyle(
+    color: Colors.deepOrange,
+    fontWeight: FontWeight.bold,
+    fontSize: 17
+  );
+
+  var textStyleBlue = const TextStyle(
+    color: Colors.lightBlue,
+    fontSize: 17
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: const Text('Android ATC Hotel'),
+        backgroundColor: Colors.orangeAccent,
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            crearCardImagen(),
+            crearCheckInCheckOut("Check-in Date: ", dateCheckIn, CHECK_IN),
+            crearCheckInCheckOut("Check-out Date: ", dateCheckOut, CHECK_OUT),
+            crearSliderAdultos(),
+            crearSliderNinios(),
+            crearTitulo("Extras:"),
+            crearCheckboxesExtras(),
+            crearTitulo("View:"),
+            crearRadioVistas(),
+            crearBoton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget crearCardImagen() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0)
+      ),
+      elevation: 10,
+      child: const Image(image: AssetImage("assets/images/banner.png")),    
+    );
+  }
+
+  Widget crearCheckInCheckOut(titulo, date, tipo) {
+    return Row(
+      children: [
+        Text(titulo, style: textStyleOrange,),
+        Text("${date.year}-${date.month}-${date.day}", style: textStyleBlue,),
+        IconButton(
+          onPressed: () {
+            selectedDate(context, tipo);
+          },
+          icon: const Icon(Icons.date_range, color: Colors.deepOrange,)
+        )
+      ],
+    );
+  }
+
+  Future<void> selectedDate(context, tipo) async {
+    final DateTime? pickerDate = await showDatePicker(
+      context: context, 
+      initialDate: (tipo == CHECK_IN) ? dateCheckIn : dateCheckOut, 
+      firstDate: DateTime.now(), 
+      lastDate: DateTime(2025)
+    );
+    if(pickerDate != null && pickerDate != dateCheckIn && pickerDate != dateCheckOut) {
+      setState(() {
+        if(tipo == CHECK_IN) {
+          dateCheckIn = pickerDate;
+        } else {
+          dateCheckOut = pickerDate;
+        }
+      });
+    }
+  }
+          
+  Widget crearSliderAdultos() {
+    return Row(
+      children: [
+        Text("Adultos: $numeroDeAdultos", style: textStyleOrange,),
+        Expanded(
+          child: Slider(
+            value: numeroDeAdultos.toDouble(), 
+            min: 1,
+            max: 6,
+            divisions: 6,
+            onChanged: (valor) {
+              setState(() {
+                numeroDeAdultos = valor.toInt();
+              });
+            }
+          ),
+        )
+      ],
+    );
+  }
+  
+  Widget crearSliderNinios() {
+    return Row(
+      children: [
+        Text("Niños: $numeroDeNinios", style: textStyleOrange,),
+        Expanded(
+          child: Slider(
+            value: numeroDeNinios.toDouble(), 
+            min: 0.0,
+            max: 6.0,
+            divisions: 6,
+            onChanged: (valor) {
+              setState(() {
+                numeroDeNinios = valor.toInt();
+              });
+            }
+          ),
+        )
+      ],
+    );
+  }
+            
+  Widget crearTitulo(titulo) {
+    return Text(titulo, style: textStyleOrange,);
+  }
+  
+  Widget crearCheckboxesExtras() {
+    return CheckboxGroup(
+      labels: const [
+        "Desayuno (Bs. 50/dia",
+        "Internet WiFi (Bs. 30/dia)",
+        "Parqueo (Bs. 20/dia)",
+        "Piscina (Bs. 100/dia)"
+      ],
+      onSelected: (List<String> list){
+        print(list);
+      },
+    );
+  }
+
+  Widget crearRadioVistas() {
+    return RadioButtonGroup(
+      labels: const [
+        "Vista al lago",
+        "Vista a la piscina",
+        "Vista a la montaña"
+      ],
+      onSelected: (seleccionado) {
+        print(seleccionado);
+      },
+    );
+  }
+  
+  Widget crearBoton() {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.pushNamed(context, RoutePaths.roomScreen);
+      },
+      style: ElevatedButton.styleFrom(backgroundColor: Colors.orangeAccent), 
+      child: const Text("Ver habitaciones", 
+          style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 17
+          ),)
     );
   }
 }
--- lesson08_lab/lib/src/screens/room_screen.dart
+++ lesson08_lab/lib/src/screens/room_screen.dart
@@ -7,14 +7,149 @@ class RoomScreen extends StatefulWidget {
   _RoomScreenState createState() => _RoomScreenState();
 }
 
+class ItemHabitacion {
+  bool isExpanded;
+  final Widget header;
+  final String title;
+  final String body;
+
+  ItemHabitacion(
+    this.isExpanded,
+    this.header,
+    this.title,
+    this.body,
+  );
+}
+
 class _RoomScreenState extends State<RoomScreen> {
+  List<ItemHabitacion> listaHabitaciones = [
+    ItemHabitacion(
+        false,
+        const Image(
+          height: 60,
+          image: AssetImage('assets/images/habitacion1.jpeg'),
+        ),
+        "Habitación grande",
+        "Habitación con una cama tamaño King"),
+    ItemHabitacion(
+        false,
+        const Image(
+          height: 60,
+          image: AssetImage('assets/images/habitacion2.webp'),
+        ),
+        "Habitación doble",
+        "Habitación con dos camas, diseñada para dos personas."),
+    ItemHabitacion(
+        false,
+        const Image(
+          height: 60,
+          image: AssetImage('assets/images/habitacion3.jpg'),
+        ),
+        "Habitación Familiar",
+        "Habitación extra grande diseñada para 3 o 4 personas."),
+  ];
+
+  ListView? listViewHabitaciones;
+
   @override
   Widget build(BuildContext context) {
+    listViewHabitaciones = ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ExpansionPanelList(
+            children: listaHabitaciones.map((ItemHabitacion item) {
+              return ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return Container(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: ListTile(
+                        leading: item.header,
+                        title: Text(
+                          item.title,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orangeAccent,
+                          ),
+                        )),
+                  );
+                },
+                isExpanded: item.isExpanded,
+                body: Container(
+                  padding: const EdgeInsets.only(
+                      bottom: 15.0, left: 15.0, right: 15.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    item.body,
+                  ),
+                ),
+              );
+            }).toList(),
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                listaHabitaciones[index].isExpanded =
+                    !listaHabitaciones[index].isExpanded;
+              });
+            },
+          ),
+        )
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rooms'),
+        backgroundColor: Colors.orangeAccent,
+        title: const Text(
+          "Lista de habitaciones",
+        ),
       ),
-      body: Container(),
+      body: listViewHabitaciones,
+      persistentFooterButtons: [
+        ButtonBar(children: [
+          ElevatedButton(
+            style:
+                ElevatedButton.styleFrom(backgroundColor: Colors.orangeAccent),
+            onPressed: () {
+              nosVemos(context);
+            },
+            child: const Text('Finalizar',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  color: Colors.white,
+                )),
+          ),
+        ]),
+      ],
     );
+  }
+
+  void nosVemos(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: const Text(
+        "Nos vemos pronto",
+        style: TextStyle(color: Colors.orangeAccent),
+      ),
+      content: const Text(
+          "Gracias por su visita. recuerda visitar nuestras redes sociales /HotelesVIP"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text(
+            "OK",
+            style: TextStyle(color: Colors.orangeAccent),
+          ),
+        )
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (context) {
+          return alertDialog;
+        });
   }
 }
