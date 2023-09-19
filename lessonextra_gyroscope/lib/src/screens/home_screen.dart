5553a8aea4182c2336f4a0398eb170b607794aaa
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2023-09-18 22:23:12 -0400

    add final code gyroscope

--- lessonextra_gyroscope/lib/src/screens/home_screen.dart
+++ lessonextra_gyroscope/lib/src/screens/home_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({ Key? key }) : super(key: key);
@@ -10,6 +11,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double x = 0, y = 0, z = 0;
+  String direction = "None";
+
+  @override
+  void initState() {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      print(event);
+      x = event.x;
+      y = event.y;
+      z = event.z;
+
+      if(x > 0) {
+        direction = "atras";
+      } else {
+        if(x < 0) {
+          direction = "adelante";
+        } else {
+          if(y > 0) {
+            direction = "izquierda";
+          } else {
+            if(y < 0) {
+              direction = "derecha";
+            } else {
+              if(z > 0) {
+                direction = "rotando a derecha";
+              } else {
+                direction = "rotando a izquierda";
+              }
+            }
+          }
+        }
+      }
+
+      setState(() {
+        direction = direction;
+      });
+    });
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) { 
@@ -22,8 +63,9 @@ class _HomeScreenState extends State<HomeScreen> {
         alignment: Alignment.center,
         padding: const EdgeInsets.all(30),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children:[
-          
+            Text(direction, style: const TextStyle(fontSize: 30),)
           ]
         )
       ),
